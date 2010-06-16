6aa9517d1966bcf2f71ab5955731cb6111380342
tfrayner <tfrayner@17793407-88c4-4385-950d-c3c3522e3e7a>
2010-06-16 14:45:37 +0000

    Continuing development of the MAGE-TAB export code.

--- trunk/lib/ClinStudy/MAGETAB/Dumper.pm
+++ trunk/lib/ClinStudy/MAGETAB/Dumper.pm
@@ -27,8 +27,11 @@ use warnings;
 use Moose;
 
 use Bio::MAGETAB;
+use Bio::MAGETAB::Util::Builder;
 use Bio::MAGETAB::Util::Writer;
 use URI;
+use LWP::UserAgent;
+use XML::LibXML;
 
 use Carp;
 use Data::Dumper;
@@ -47,29 +50,188 @@ has 'password' => ( is       => 'ro',
                     isa      => 'Str',
                     required => 1, );
 
-has 'magetab' => ( is       => 'ro',
-                   isa      => 'Bio::MAGETAB',
-                   required => 1,
-                   default  => sub { Bio::MAGETAB->new() }, );
+has 'builder'  => ( is       => 'ro',
+                    isa      => 'Bio::MAGETAB::Util::Builder',
+                    required => 1,
+                    default  => sub { Bio::MAGETAB::Util::Builder->new() }, );
 
 sub dump {
 
     my ( $self ) = @_;
 
-    # Sort objects into SDRFRows FIXME.
+    # FIXME whence $sdrf_file?
+    my $sdrf_file = 'testing.sdrf';
+
+    # Sort objects into SDRFRows.
+    my $sdrf = $self->builder->find_or_create_sdrf({
+        uri => $sdrf_file,
+    });
+    $sdrf->add_nodes( [ $self->builder->get_magetab()->get_nodes() ] );
 
     # Write out all objects.
-    my $writer = Bio::MAGETAB::Util::Writer->new( magetab => $magetab );
+    my $writer = Bio::MAGETAB::Util::Writer->new(
+        magetab => $self->builder->get_magetab(),
+    );
 
     $writer->write();
 
     return;
 }
 
+sub _query_rest {
+
+    my ( $self, $querytype, $querystr ) = @_;
+
+    my $uri = $self->uri();
+
+    # Strip trailing /, add the path to the REST API for query type
+    # (typically 'assay_file').
+    $uri =~ s/\/+$//;
+    $uri .= sprintf("/rest/%s/%s", $querytype, $querystr);
+    my $ua = LWP::UserAgent->new();
+
+    # Add our login details and content type.
+    $ua->default_header( 'X-Username' => $self->username() );
+    $ua->default_header( 'X-Password' => $self->password() );
+    $ua->default_header( 'Content-Type' => 'text/xml' );
+
+    # Get the response.
+    my $res = $ua->get($uri);
+
+    # Error-check the response, give feedback on failure.
+    unless ( $res->is_success() ) {
+        warn(sprintf("Warning: ClinWebQuery returned error %s for %s %s\n",
+                     $res->status_line, $querytype, $querystr));
+        return;
+    }
+
+    # Parse the returned data, remove the unnecessary top-level wrappers.
+    my $doc     = XML::LibXML->load_xml(string => $res->content);
+    my $dataset = $doc->find('/opt/data');
+    if ( $dataset->size() != 1 ) {
+        croak("Error: ClinStudyWeb REST response must contain a single data entry");
+    }
+    my $data = $dataset->get_node(0);
+
+    return $data;
+}
+
+sub _create_datafile {
+
+    my ( $self, $filename ) = @_;
+
+    # FIXME this should be a config setting.
+    my %extmap = (
+        'cel' => 'Affymetrix CEL',
+        'chp' => 'Affymetrix CHP',
+        'txt' => 'Unknown Text',
+        'gpr' => 'GeneSpring',
+    );
+
+    my $dformat_str;
+    my ( $filext ) = ( $filename =~ /\. \w{3,5} \z/ixms );
+    if ( $filext ) {
+        unless ( $dformat_str = $extmap{ lc( $filext ) } ) {
+            $dformat_str = 'Unknown';
+        }
+    }
+    else {
+        $dformat_str = 'Unknown';
+    }
+
+    my $builder = $self->builder();
+
+    my $dtype = $builder->find_or_create_controlled_term({
+        category => 'DataType',
+        value    => 'raw',
+    });
+
+    my $dformat = $builder->find_or_create_controlled_term({
+        category => 'DataFormat',
+        value    => $dformat_str,
+    });
+
+    # Data file
+    my $file = $builder->find_or_create_data_file({
+        uri        => $filename,
+        dataType   => $dtype,
+        format     => $dformat,
+    });
+
+    return $file;
+}
+
 sub add_file {
 
     my ( $self, $filename ) = @_;
 
+    my $xmldata = $self->_query_rest( 'assay_file', $filename );
+
+    my $builder = $self->builder();
+
+    # Data file
+    my $file = $self->_create_datafile( $filename );
+
+    # Hybridization
+    my $ttype = $builder->find_or_create_controlled_term({
+        category => 'TechnologyType',
+        value    => 'hybridization',
+    });
+
+    my $hybid = $xmldata->getAttribute('identifier');
+    my $hyb = $builder->find_or_create_assay({
+        name           => $hybid,
+        technologyType => $ttype,
+    });
+    $builder->find_or_create_edge({
+        inputNode  => $hyb,
+        outputNode => $file,
+    });
+
+    foreach my $channel ( $xmldata->findnodes('./channels') ) {
+        my $label_str = $channel->getAttribute('label');
+        my $label     = $builder->find_or_create_controlled_term({
+            category => 'LabelCompound',
+            value    => $label_str,
+        });
+
+        foreach my $sample ( $channel->findnodes('./sample' ) ) {
+            my $sample_name = $sample->getAttribute('sample_name');
+            my $le = $builder->find_or_create_labeled_extract({
+                name  => "$sample_name ($label_str)",
+                label => $label,
+            });
+            my $ex = $builder->find_or_create_extract({
+                name  => $sample_name,
+            });
+            my $sa = $builder->find_or_create_sample({
+                name  => $sample_name,
+            });
+            my $so = $builder->find_or_create_source({
+                name  => $sample->getAttribute('patient_number'),
+            });
+
+            # Create the edges also.
+            $builder->find_or_create_edge({
+                inputNode  => $so,
+                outputNode => $sa,
+            });
+            $builder->find_or_create_edge({
+                inputNode  => $sa,
+                outputNode => $ex,
+            });
+            $builder->find_or_create_edge({
+                inputNode  => $ex,
+                outputNode => $le,
+            });
+            $builder->find_or_create_edge({
+                inputNode  => $le,
+                outputNode => $hyb,
+            });
+        }
+    }
+
+    return;
 }
 
 1;
@@ -117,9 +279,9 @@ The username to use in connecting to the database.
 
 The password to use in connecting to the database.
 
-=item magetab
+=item builder
 
-A Bio::MAGETAB container object. This will be created automatically by
+A Bio::MAGETAB::Util::Builder object. This will be created automatically by
 default; this option should not typically be used unless you know what
 you're doing.
 
