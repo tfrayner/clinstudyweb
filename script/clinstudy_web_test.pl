ea2db091c4a060b86c8d48be58714281a27d22a8
Tim Rayner <tfrayner@gmail.com>
2010-06-24 14:02:13 +0000

    Regular automated update of catalyst scripts.

--- script/clinstudy_web_test.pl
+++ script/clinstudy_web_test.pl
@@ -1,23 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'ClinStudy::Web';
-
-require Digest::SHA;
-require DBD::mysql;
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('ClinStudy::Web', 'Test');
 
 1;
 
@@ -30,7 +14,7 @@ clinstudy_web_test.pl - Catalyst Test
 clinstudy_web_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    clinstudy_web_test.pl http://localhost/some_action
