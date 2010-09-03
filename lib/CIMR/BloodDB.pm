3354d7a909ed34326389d440088761c9696060c0
Tim Rayner <tfrayner@gmail.com>
2010-09-03 14:34:01 +0000

    Updating CIMR-specific module docs; also deleted obsolete CIMR::AutoDB class.

--- lib/CIMR/BloodDB.pm
+++ lib/CIMR/BloodDB.pm
@@ -17,3 +17,40 @@ __PACKAGE__->load_classes;
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
+
+=head1 NAME
+
+CIMR::BloodDB - Local CIMR extension for interacting with blood test database.
+
+=head1 SYNOPSIS
+
+ use CIMR::BloodDB;
+ my $schema = CIMR::BloodDB->connect(
+     sprintf("dbi:SQLite:dbname=%s", $filename ),
+ );
+
+=head1 DESCRIPTION
+
+This is an object-relational mapping (ORM) class, based on
+DBIx::Class, used to connect to a local SQLite database containing
+blood test results. These results will typically have been imported
+from their original MS Access database file using
+C<CIMR::AccessImporter>.
+
+=head1 SEE ALSO
+
+L<CIMR::AccessImporter>
+
+=head1 AUTHOR
+
+Tim F. Rayner <tfrayner@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010 by Tim F. Rayner, University of Cambridge
+
+This library is released under version 2 of the GNU General Public
+License (GPL).
+
+=cut
+
