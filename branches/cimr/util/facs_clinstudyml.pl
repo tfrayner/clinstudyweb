444702bfc2088ec504adb6cbde3bf3791b7589a4
tfrayner <tfrayner@17793407-88c4-4385-950d-c3c3522e3e7a>
2011-10-17 15:44:05 +0000

    Moderately complete but untested script.

--- branches/cimr/util/facs_clinstudyml.pl
+++ branches/cimr/util/facs_clinstudyml.pl
@@ -24,16 +24,19 @@ use warnings;
 
 use Getopt::Long;
 use Pod::Usage;
-
-use Data::Dumper;
+use Text::CSV_XS;
+use Digest::MD5;
+use File::Spec;
+use File::Copy;
 
 sub parse_args {
 
-    my ( $mapfile, $want_help );
+    my ( $mapfile, $want_help, $targetdir );
 
     GetOptions(
-        "m|mapfile=s" => \$mapfile,
-        "h|help"      => \$want_help,
+        "m|mapfile=s"   => \$mapfile,
+        "d|targetdir=s" => \$targetdir,
+        "h|help"        => \$want_help,
     );
 
     if ($want_help) {
@@ -44,7 +47,7 @@ sub parse_args {
         );
     }
 
-    unless ( $mapfile && scalar @ARGV ) {
+    unless ( $mapfile && $targetdir && scalar @ARGV ) {
         pod2usage(
             -message => qq{Please see "$0 -h" for further help notes.},
             -exitval => 255,
@@ -53,20 +56,168 @@ sub parse_args {
         );
     }
 
-    return( $mapfile, \@ARGV );
+    return( $mapfile, $targetdir, \@ARGV );
 }
 
 sub parse_mapfile {
 
     my ( $mapfile ) = @_;
 
-    
+    open( my $fh, '<', $mapfile )
+        or die("Unable to open map file $mapfile: $!\n");
+
+    my $csv = Text::CSV_XS->new({
+        sep_char  => "\t",
+        eol       => "\n",
+    }) or die("Unable to initialise CSV parser.");
+
+    my ( $headstr, $header );
+    until ( $headstr && $headstr !~ /^\s*#/ ) {
+        $header = $csv->getline($fh);
+        unless ( $header && ref $header eq 'ARRAY' ) {
+            die("Unable to read file header line.\n");
+        }
+        $headstr = join('', @$header);
+    }
+
+    # Strip whitespace on either side of each column header.
+    $header = [ map { s/ \A \s* (.*?) \s* \z /$1/ixms; $_ } @$header ];
+
+    my %map;
+
+    LINE:
+    while ( my $line = $csv->getline($fh) ) {
+        
+        my $str = join('', @$line);
+        next LINE if $str =~ /^\s*#/;
+        
+        my %col;
+        @col{@$header} = @$line;
+
+        $map{ $col{'label'} } = {
+            triad  => $col{'triad'},
+            vdate  => $col{'vdate'},
+        };
+    }
+
+    # Check that parsing completed successfully.
+    my ( $error, $mess ) = $csv->error_diag();
+    unless ( $error == 2012 ) {    # 2012 is the Text::CSV_XS EOF code.
+        die(
+            sprintf(
+                "Error in tab-delimited format: %s. Bad input was:\n\n%s\n",
+                $mess,
+                $csv->error_input(),
+            ),
+        );
+    }
+
+    return \%map;
 }
 
-my ( $mapfile, $dirs ) = parse_args();
+sub md5_file_digest {
 
+    my ( $file ) = @_;
 
+    open ( my $fh, '<', $file )
+        or die("Unable to open FACS file $file: $!\n");
 
+    my $md5 = Digest::MD5->new();
+    my $chunk;
+    my $chunksize = 65536;    # 64k for reasonable efficiency (untested though).
+    while ( my $bytes = read( $fh, $chunk, $chunksize ) ) {
+	$md5->add( $chunk );
+    }
+
+    return $md5->hexdigest();
+}
+
+sub process_file {
+
+    my ( $file, $vhash ) = @_;
+
+    my ( $ctype_cv, $file_type ) = ( $file =~ /^(\w+) ([^.]+)/ );
+
+    unless ( $ctype_cv && $file_type ) {
+        die("Unable to parse cell and file type from filename: $file\n");
+    }
+
+    my $ftype_cv;
+    if ( $file_type eq 'pre' ) {
+        $ftype_cv = 'FACS pre';
+    }
+    elsif ( $file_type eq '+ve' ) {
+        $ftype_cv = 'FACS positive';
+    }
+    else {
+        return;
+    }
+
+    my $md5 = md5_file_digest( $file );
+
+    return( "${md5}.fcs", $ctype_cv, $ftype_cv );
+}
+
+sub process_directory {
+
+    my ( $dir, $targetdir, $vhash ) = @_;
+
+    opendir( my $dh, $dir )
+        or die("Cannot open directory $dir: $!\n");
+
+    my @files = grep { /^[^.]/ && -f "$dir/$_" } readdir($dh);
+    closedir $dh;
+
+    FILE:
+    foreach my $file ( @files ) {
+        next FILE if $file =~ /^Unstained/;
+        my ( $md5file, $celltype, $filetype )
+            = process_file( File::Spec->catfile( $dir, $file ), $vhash );
+
+        # Copy the $file to $md5file in a target directory.
+        copy( $file, File::Spec->catfile( $targetdir, $md5file ) )
+            or die("Unable to copy FACS data file $file to target directory: $!");
+
+        print STDOUT (join("\t",
+                           $md5file,
+                           $filetype,
+                           $celltype,
+                           'RNA',
+                           $vhash->{'triad'},
+                           $vhash->{'vdate'}), "\n")
+    }
+}
+
+########
+# MAIN #
+########
+
+my ( $mapfile, $targetdir, $dirs ) = parse_args();
+
+my $map = parse_mapfile( $mapfile );
+
+# Quick pre-check that all the directories on the command line can be
+# mapped to patient visits.
+my %not_found;
+foreach my $dir ( @$dirs ) {
+    $not_found{$dir}++ unless ( exists $map->{ $dir } );
+}
+if ( scalar grep { defined $_ } values %not_found ) {
+    die(join("\n",
+             "Directories not found in the map file: ", keys %not_found));
+}
+
+print STDOUT (join("\t",
+                   'SampleData|filename',
+                   'SampleData|type',
+                   'Sample|cell_type',
+                   'Sample|material_type',
+                   'Patient|trial_id',
+                   'Visit|date'), "\n");
+
+foreach my $dir ( @$dirs ) {
+    process_directory( $dir, $targetdir, $map->{ $dir } );
+}
 
 __END__
 
