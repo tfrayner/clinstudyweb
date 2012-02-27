685bf2daf67009c99087e3b996dc16d6aaa8a73c
tfrayner <tfrayner@17793407-88c4-4385-950d-c3c3522e3e7a>
2012-02-27 10:43:54 +0000

    Adding a couple of *_audit_history columns which were missed while making recent changes.

--- branches/dev/schema/migration/migrate_v0_2_to_v0_3.sql
+++ branches/dev/schema/migration/migrate_v0_2_to_v0_3.sql
@@ -411,8 +411,10 @@ delimiter ;
 -- Add a much-needed transplant notes field.
 --
 alter table transplant add column notes text;
+alter table transplant_audit_history add column notes text;
 
 --
 -- A column for our automated cell purity assessment counts (this is actually on the CIMR branch).
 --
 alter table sample add column auto_cell_purity decimal(12,5) default NULL;
+alter table sample_audit_history add column auto_cell_purity decimal(12,5) default NULL;
